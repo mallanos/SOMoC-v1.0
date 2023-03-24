a9f7dc9fb205f6680230fc7f55d28431656534bb
mallanos <manu.llanos@gmail.com>
2023-03-24 10:00:53 -0300

    create a clustering module

--- SOMoC_v1.0_standalone.py
+++ SOMoC_v1.0_standalone.py
@@ -9,23 +9,18 @@
 import pandas as pd
 import time
 from typing import List, Tuple, Union, Optional, Dict, Any
-from datetime import date
-from pathlib import Path
 import numpy as np
 import logging
 from tqdm import tqdm
-from datetime import datetime
 import argparse
 
-from sklearn.mixture import GaussianMixture as GMM
-from sklearn.metrics import silhouette_score, silhouette_samples, calinski_harabasz_score, davies_bouldin_score, pairwise_distances
-from validclust import dunn
 import umap
 from rdkit import Chem
 from rdkit.Chem.EState.Fingerprinter import FingerprintMol
 
 from modules.data import *
 from modules.plotting import *
+from modules.clustering import *
 
 def Fingerprints_calculator(data: pd.DataFrame) -> np.ndarray:
     """
@@ -109,173 +104,6 @@ def UMAP_reduction(X: np.ndarray, settings) -> Tuple[np.ndarray, int]:
 
     return embedding, n_components
 
-def Calculate_CVIs(embeddings: np.ndarray, labels: List, Random: bool = True, num_iterations: int = 500, num_clusters: int = 3):
-
-    """
-    Calculate all CVIs for real or random clustering
-
-    Parameters:
-    embeddings (np.ndarray): An array of embeddings to cluster.
-    labels (List): List of cluster labels.
-    
-    Returns: A nested dictionary containing the all CVIs
-    """
-
-    results = {}
-
-    if Random:
-        SILs = np.zeros(num_iterations)
-        DBs = np.zeros(num_iterations)
-        CHs = np.zeros(num_iterations)
-        DUNNs = np.zeros(num_iterations)
-
-        for i in range(num_iterations):
-            np.random.seed(i)
-            random_clusters = np.random.randint(num_clusters, size=len(embeddings))
-            silhouette_random = silhouette_score(embeddings, random_clusters)
-            SILs[i] = silhouette_random
-            db_random = davies_bouldin_score(embeddings, random_clusters)
-            DBs[i] = db_random
-            ch_random = calinski_harabasz_score(embeddings, random_clusters)
-            CHs[i] = ch_random
-            dist_dunn = pairwise_distances(embeddings)
-            dunn_random = dunn(dist_dunn, random_clusters)
-            DUNNs[i] = dunn_random
-
-        sil_random = np.mean(SILs).round(4)
-        sil_random_st = np.std(SILs).round(4)
-        db_random = np.mean(DBs).round(4)
-        db_random_st = np.std(DBs).round(4)
-        ch_random = np.mean(CHs).round(4)
-        ch_random_st = np.std(CHs).round(4)
-        dunn_random = np.mean(DUNNs).round(4)
-        dunn_random_st = np.std(DUNNs).round(4)
-
-        random_means = [sil_random, db_random, ch_random, dunn_random]
-        random_sds = [sil_random_st, db_random_st, ch_random_st, dunn_random_st]
-        
-        results = pd.DataFrame({'Random_mean': random_means,'Random_std':random_sds}, index=['silhouette','davies_bouldin','calinski_harabasz','dunn'])
-
-    else:
-        assert len(embeddings) == len(labels), "Length of embeddings and labels must match"
-        assert embeddings.ndim == 2, "Embeddings must be a 2D array"
-
-        results['SOMoC'] = {
-            'silhouette': silhouette_score(embeddings, labels, metric='cosine').round(4),
-            'davies_bouldin': davies_bouldin_score(embeddings, labels).round(4),
-            'calinski_harabasz': calinski_harabasz_score(embeddings, labels).round(4),
-            'dunn': dunn(pairwise_distances(embeddings), labels).round(4)
-        }
-        results = pd.DataFrame.from_dict(results)
-
-    return results
-
-def GMM_clustering_loop(embeddings: np.ndarray, settings) -> Tuple[pd.DataFrame, int]:
-    """
-    Runs GMM clustering for a range of K values and returns the K value which maximizes the silhouette score.
-
-    Parameters:
-    embeddings (np.ndarray): An array of embeddings to cluster.
-    max_n_clusters (int): The maximum number of K values to try. Default is 10.
-    iterations (int): The number of iterations to run for each K value. Default is 10.
-    n_init (int): The number of initializations to perform for each K value. Default is 10.
-    init_params (str): The method to initialize the model parameters. Default is 'kmeans'.
-    covariance_type (str): The type of covariance to use. Default is 'full'.
-    warm_start (bool): Whether to reuse the previous solution as the initialization for the next K value. Default is False.
-
-    Returns:
-    Tuple[pd.DataFrame, int]: A tuple of the results dataframe and the K value which maximizes the silhouette score.
-    """
-    max_n_clusters = settings.gmm['max_n_clusters']
-    iterations = settings.gmm['iterations']
-    n_init = settings.gmm['n_init']
-    init_params = settings.gmm['init_params']
-    covariance_type = settings.gmm['covariance_type']
-    warm_start = settings.gmm['warm_start']
-
-    logging.info("SOMoC will try to find the optimal K")
-
-    temp = {i: [] for i in range(max_n_clusters+1)}  # pre-allocate the dictionary
-
-    for n in tqdm(range(2, max_n_clusters+1), desc='Optimizing the number of cluters'):
-        temp_sil = [None] * iterations # pre-allocate the list
-        for x in range(iterations):
-            gmm = GMM(n, n_init=n_init, init_params=init_params, covariance_type=covariance_type,
-                      warm_start=warm_start, random_state=x, verbose=0).fit(embeddings)
-            labels = gmm.predict(embeddings)
-            temp_sil[x] = silhouette_score(
-                embeddings, labels, metric='cosine')
-        temp[n] = [int(n),np.mean(temp_sil), np.std(temp_sil)]
-
-    results = pd.DataFrame.from_dict(
-        temp, orient='index', columns=['Clusters','Silhouette', 'sil_stdv']).dropna()
-    results = results.astype({"Clusters": int})
-    results_sorted = results.sort_values(['Silhouette'], ascending=False)
-    K_loop = results_sorted.index[0]  # Get max Sil K
-    
-    return results, int(K_loop)
-
-def GMM_clustering_final(embeddings: np.array, settings, K: int= 3):
-    
-    """
-    Cluster the dataset using the optimal K value, and calculate several CVIs
-    Parameters:
-    embeddings (array-like): The input data to cluster.
-    K (int): The number of clusters to form.
-    n_init (int): The number of initializations to perform.
-    init_params (str): The method used to initialize the weights, means, and covariances.
-    warm_start (bool): Whether to reuse the last solution to initialize the next fit.
-    covariance_type (str): The type of covariance parameters to use.
-    random_state (int): The random seed to use.
-
-    Returns:
-        data_clustered (pandas.DataFrame): The input data with an additional 'cluster' column.
-    """
-    n_init = settings.gmm['n_init']
-    init_params = settings.gmm['init_params']
-    covariance_type = settings.gmm['covariance_type']
-    warm_start = settings.gmm['warm_start']
-    random_state = settings.random_state
-
-    logging.info(f'Running final clustering with K = {K}')
-
-    GMM_final = GMM(K, n_init=n_init, init_params=init_params, warm_start=warm_start,
-                    covariance_type=covariance_type, random_state=random_state, verbose=0)
-    GMM_final.fit(embeddings)
-    labels_final = GMM_final.predict(embeddings)
-
-    if GMM_final.converged_:
-        logging.info('GMM converged.')
-    else:
-        logging.warning('GMM did not converge. Please check you input configuration.')
-       
-    logging.info('Calculating CVIs')
-    results_real = Calculate_CVIs(embeddings, labels=labels_final, Random = False)
-    # create DataFrame from real results
-    df_real = pd.DataFrame.from_dict(results_real, orient='columns')
-    results_random = Calculate_CVIs(embeddings, labels=None, num_clusters=K, Random = True)
-    # create DataFrame from random results
-    df_random = pd.DataFrame.from_dict(results_random, orient='columns')
-    # concatenate DataFrames along columns axis
-    results_CVIs = pd.concat([df_real, df_random], axis=1)
-
-    cluster_final = pd.DataFrame({'cluster': labels_final}, index=data.index)
-    results_clustered = data.join(cluster_final)
-
-    if 'mol' in results_clustered.columns:  # Check if mol column from standardization is present
-        try:
-            results_clustered['SMILES_standardized'] = results_clustered['mol'].apply(
-                lambda x: Chem.MolToSmiles(x))
-            results_clustered.drop(['mol'], axis=1, inplace=True)
-        except Exception as e:
-            logging.warning('Something went wrong converting standardized molecules back to SMILES code..: {e}')
-            pass
-
-    results_clustered.to_csv(f'results/{name}/{name}_Clustered.csv', index=True, header=True)
-    results_CVIs.to_csv(f'results/{name}/{name}_CVIs.csv', index=True, header=True)
-
-    return results_clustered, results_CVIs, GMM_final
-
 ####################################### SOMoC main ########################################
 ###########################################################################################
 if __name__ == '__main__':
@@ -311,7 +139,7 @@ if __name__ == '__main__':
     # Create output dir 
     make_dir(f'results/{name}') 
     
-    # Convert smiles to RDKit molecule
+    # Convert SMILES to RDKit molecule
     data = data_handler.smiles_to_mol(data=data_raw, standardize=settings.standardize_molec)
     
     # Calculate Fingerprints
@@ -320,13 +148,15 @@ if __name__ == '__main__':
     # Reduce feature space with UMAP
     embedding, n_components = UMAP_reduction(X, settings)
    
+    cluster = Clustering(name, embedding, settings)
+
     if settings.optimal_K is not False:
         # Run the clustering and calculate all CVIs
-        results_clustered, results_CVIs, results_model = GMM_clustering_final(embedding, settings, K=settings.optimal_K)
+        results_clustered, results_CVIs, results_model = cluster.GMM_final(K=settings.optimal_K)
     else:
         # If optimal_K is not set, run the GMM clustering loop to get K
-        results_loop, optimal_K = GMM_clustering_loop(embedding, settings)
-        results_clustered, results_CVIs, results_model = GMM_clustering_final(embedding, settings, K=optimal_K)
+        results_loop, optimal_K = cluster.GMM_loop()
+        results_clustered, results_CVIs, results_model = cluster.GMM_final(K=optimal_K)
         # Update the original JSON file
         settings.optimal_K = optimal_K
         # Generate the elbow plot   
---
+++ modules/clustering.py
@@ -0,0 +1,168 @@
+import pandas as pd
+import numpy as np
+import logging
+from tqdm import tqdm
+from typing import List, Tuple, Union, Optional, Dict, Any
+
+from sklearn.mixture import GaussianMixture as GMM
+from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score, pairwise_distances
+from validclust import dunn
+
+def calculate_CVIs(embedding: np.ndarray, labels: List, Random: bool = True, num_iterations: int = 500, num_clusters: int = 3):
+
+    """
+    Calculate all CVIs for real or random clustering
+
+    Parameters:
+    embeddings (np.ndarray): An array of embeddings to cluster.
+    labels (List): List of cluster labels.
+    
+    Returns: A nested dictionary containing the all CVIs
+    """
+
+    results = {}
+
+    if Random:
+        SILs = np.zeros(num_iterations)
+        DBs = np.zeros(num_iterations)
+        CHs = np.zeros(num_iterations)
+        DUNNs = np.zeros(num_iterations)
+
+        for i in range(num_iterations):
+            np.random.seed(i)
+            random_clusters = np.random.randint(num_clusters, size=len(embedding))
+            silhouette_random = silhouette_score(embedding, random_clusters)
+            SILs[i] = silhouette_random
+            db_random = davies_bouldin_score(embedding, random_clusters)
+            DBs[i] = db_random
+            ch_random = calinski_harabasz_score(embedding, random_clusters)
+            CHs[i] = ch_random
+            dist_dunn = pairwise_distances(embedding)
+            dunn_random = dunn(dist_dunn, random_clusters)
+            DUNNs[i] = dunn_random
+
+        sil_random = np.mean(SILs).round(4)
+        sil_random_st = np.std(SILs).round(4)
+        db_random = np.mean(DBs).round(4)
+        db_random_st = np.std(DBs).round(4)
+        ch_random = np.mean(CHs).round(4)
+        ch_random_st = np.std(CHs).round(4)
+        dunn_random = np.mean(DUNNs).round(4)
+        dunn_random_st = np.std(DUNNs).round(4)
+
+        random_means = [sil_random, db_random, ch_random, dunn_random]
+        random_sds = [sil_random_st, db_random_st, ch_random_st, dunn_random_st]
+        
+        results = pd.DataFrame({'Random_mean': random_means,'Random_std':random_sds}, index=['silhouette','davies_bouldin','calinski_harabasz','dunn'])
+
+    else:
+        assert len(embedding) == len(labels), "Length of embeddings and labels must match"
+        assert embedding.ndim == 2, "Embeddings must be a 2D array"
+
+        results['SOMoC'] = {
+            'silhouette': silhouette_score(embedding, labels, metric='cosine').round(4),
+            'davies_bouldin': davies_bouldin_score(embedding, labels).round(4),
+            'calinski_harabasz': calinski_harabasz_score(embedding, labels).round(4),
+            'dunn': dunn(pairwise_distances(embedding), labels).round(4)
+        }
+        results = pd.DataFrame.from_dict(results)
+
+    return results
+
+class Clustering():
+
+    def __init__(self, name: str, embedding: np.ndarray, settings) -> None:
+        self.name = name
+        self.embedding = embedding
+        self.settings = settings
+
+    def GMM_loop(self) -> Tuple[pd.DataFrame, int]:
+        """
+        Runs GMM clustering for a range of K values and returns the K value which maximizes the silhouette score.
+
+        Parameters:
+        embeddings (np.ndarray): An array of embeddings to cluster.
+        max_n_clusters (int): The maximum number of K values to try. Default is 10.
+        iterations (int): The number of iterations to run for each K value. Default is 10.
+        n_init (int): The number of initializations to perform for each K value. Default is 10.
+        init_params (str): The method to initialize the model parameters. Default is 'kmeans'.
+        covariance_type (str): The type of covariance to use. Default is 'full'.
+        warm_start (bool): Whether to reuse the previous solution as the initialization for the next K value. Default is False.
+
+        Returns:
+        Tuple[pd.DataFrame, int]: A tuple of the results dataframe and the K value which maximizes the silhouette score.
+        """
+        max_n_clusters = self.settings.gmm['max_n_clusters']
+        iterations = self.settings.gmm['iterations']
+        n_init = self.settings.gmm['n_init']
+        init_params = self.settings.gmm['init_params']
+        covariance_type = self.settings.gmm['covariance_type']
+        warm_start = self.settings.gmm['warm_start']
+
+        logging.info("SOMoC will try to find the optimal K")
+
+        temp = {i: [] for i in range(max_n_clusters+1)}  # pre-allocate the dictionary
+
+        for n in tqdm(range(2, max_n_clusters+1), desc='Optimizing the number of cluters'):
+            temp_sil = [None] * iterations # pre-allocate the list
+            for x in range(iterations):
+                gmm = GMM(n, n_init=n_init, init_params=init_params, covariance_type=covariance_type,
+                        warm_start=warm_start, random_state=x, verbose=0).fit(self.embedding)
+                labels = gmm.predict(self.embedding)
+                temp_sil[x] = silhouette_score(
+                    self.embedding, labels, metric='cosine')
+            temp[n] = [int(n),np.mean(temp_sil), np.std(temp_sil)]
+
+        results = pd.DataFrame.from_dict(
+            temp, orient='index', columns=['Clusters','Silhouette', 'sil_stdv']).dropna()
+        results = results.astype({"Clusters": int})
+        results_sorted = results.sort_values(['Silhouette'], ascending=False)
+        K_loop = results_sorted.index[0]  # Get max Sil K
+        
+        return results, int(K_loop)
+
+    def GMM_final(self, K: int= 3):
+        
+        """
+        Cluster the dataset using the optimal K value, and calculate several CVIs
+        Parameters:
+        embeddings (array-like): The input data to cluster.
+        K (int): The number of clusters to form.
+        n_init (int): The number of initializations to perform.
+        init_params (str): The method used to initialize the weights, means, and covariances.
+        warm_start (bool): Whether to reuse the last solution to initialize the next fit.
+        covariance_type (str): The type of covariance parameters to use.
+        random_state (int): The random seed to use.
+
+        Returns:
+            data_clustered (pandas.DataFrame): The input data with an additional 'cluster' column.
+        """
+        n_init = self.settings.gmm['n_init']
+        init_params = self.settings.gmm['init_params']
+        covariance_type = self.settings.gmm['covariance_type']
+        warm_start = self.settings.gmm['warm_start']
+        random_state = self.settings.random_state
+
+        logging.info(f'Running final clustering with K = {K}')
+
+        GMM_final = GMM(K, n_init=n_init, init_params=init_params, warm_start=warm_start,
+                        covariance_type=covariance_type, random_state=random_state, verbose=0)
+        GMM_final.fit(self.embedding)
+        labels_final = GMM_final.predict(self.embedding)
+
+        if GMM_final.converged_:
+            logging.info('GMM converged.')
+        else:
+            logging.warning('GMM did not converge. Please check you input configuration.')
+        
+        results_clustered = pd.DataFrame({'cluster': labels_final})
+
+        logging.info('Calculating CVIs')
+        results_real = calculate_CVIs(self.embedding, labels=labels_final, Random = False)
+        results_random = calculate_CVIs(self.embedding, labels=None, num_clusters=K, Random = True)
+        # concatenate DataFrames along columns axis
+        results_CVIs = pd.concat([results_real, results_random], axis=1)
+        
+        results_CVIs.to_csv(f'results/{self.name}/{self.name}_CVIs.csv', index=True, header=True)
+
+        return results_clustered, results_CVIs, GMM_final
